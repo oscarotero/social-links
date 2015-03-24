ff0f9a4176472a7d21459df1bde47cdcf09c3d79
oscarotero <oom@oscarotero.com>
2015-03-24 10:09:54 +0100

    added Tumblr provider

---
+++ src/Providers/Tumblr.php
@@ -0,0 +1,22 @@
+<?php
+namespace SocialLinks\Providers;
+
+class Tumblr extends ProviderBase implements ProviderInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function shareUrl()
+    {
+        return $this->buildUrl(
+            'https://www.tumblr.com/share',
+            array(
+                'url' => 'u',
+                'title' => 't',
+            ),
+            array(
+                'v' => 3
+            )
+        );
+    }
+}
