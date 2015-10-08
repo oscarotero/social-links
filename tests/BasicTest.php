b2178bbd835a6127c27543acbb27a011b683b719
oscarotero <oom@oscarotero.com>
2015-10-09 00:24:17 +0200

    updated facebook test

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -44,7 +44,7 @@ class BasicTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($page->chuza->shareUrl, 'http://chuza.gl/submit.php?url=http%3A%2F%2Fmypage.com');
         $this->assertEquals($page->email->shareUrl, 'mailto:?subject=Page%20title&body=Extended%20page%20description%20%26%0Ahttp%3A%2F%2Fmypage.com');
         $this->assertEquals($page->evernote->shareUrl, 'https://www.evernote.com/clip.action?url=http%3A%2F%2Fmypage.com&title=Page+title&body=Extended+page+description+%26');
-        $this->assertEquals($page->facebook->shareUrl, 'https://www.facebook.com/sharer.php?s=100&p%5Burl%5D=http%3A%2F%2Fmypage.com&p%5Btitle%5D=Page+title&p%5Bsummary%5D=Extended+page+description+%26&p%5Bimages%5D%5B0%5D=http%3A%2F%2Fmypage.com%2Fimage.png');
+        $this->assertEquals($page->facebook->shareUrl, 'https://www.facebook.com/sharer/sharer.php?display=popup&redirect_uri=http%3A%2F%2Fwww.facebook.com&u=http%3A%2F%2Fmypage.com&t=Page+title');
         $this->assertEquals($page->linkedin->shareUrl, 'https://www.linkedin.com/shareArticle?mini=1&url=http%3A%2F%2Fmypage.com&title=Page+title&summary=Extended+page+description+%26');
         $this->assertEquals($page->liveinternet->shareUrl, 'http://www.liveinternet.ru/journal_post.php?action=n_add&cnurl=http%3A%2F%2Fmypage.com&cntitle=Page+title');
         $this->assertEquals($page->livejournal->shareUrl, 'http://www.livejournal.com/update.bml?event=%3Ca+href%3D%22http%3A%2F%2Fmypage.com%22%3EPage+title%3C%2Fa%3E&subject=Page+title');
