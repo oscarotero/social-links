3bda4a261c68ffae0bb6be44ec45c48785f9e76e
Oscar Otero <oom@oscarotero.com>
2024-08-14 18:14:27 +0200

    Added test

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -74,6 +74,7 @@ class BasicTest extends TestCase
         $this->assertEquals($page->telegram->shareUrl, 'tg://msg?text=Page+title+http%3A%2F%2Fmypage.com');
         $this->assertEquals($page->xing->shareUrl, 'https://www.xing.com/spi/shares/new?url=http%3A%2F%2Fmypage.com');
         $this->assertEquals($page->viadeo->shareUrl, 'https://partners.viadeo.com/share?url=http%3A%2F%2Fmypage.com&comment=Page+title');
+        $this->assertEquals($page->threads->shareUrl, 'https://threads.net/intent/post?text=Page+title&url=http%3A%2F%2Fmypage.com');
     }
 
     /**
