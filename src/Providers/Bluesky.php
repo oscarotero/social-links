2d7daf1a510b83ff3e624c9f68bf02921f9f681e
Mathieu <mathieu@fellicht.nl>
2024-11-27 09:33:14 +0100

    Add Bluesky

---
+++ src/Providers/Bluesky.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace SocialLinks\Providers;
+
+class Bluesky extends ProviderBase implements ProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function shareUrl()
+    {
+        $data = $this->page->get(array('title'));
+        $text = isset($data['title']) ? trim($data['title']) : '';
+
+        return $this->buildUrl(
+            'https://bsky.app/intent/compose',
+            array('url'),
+            array('text' => $text)
+        );
+    }
+}
